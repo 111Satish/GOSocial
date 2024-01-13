3d61501648c354603483f64f7802fa10db5a1aed
111Satish <satishjnvr@gmail.com>
2024-01-13 17:18:29 +0530

    user Profile component created and dummy data added

---
+++ Components/userProfile.js
@@ -0,0 +1,95 @@
+
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const UserProfile = () => {
+    const user = {
+        name: 'Satish Kumar',
+        profilePic: 'https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic.jpg',
+        city: 'Trichy',
+        occupation: 'Student',
+        connectionStatus: '+ INVITE',
+        distance: '100-200',
+        profileScore: 8,
+        interests: ['Coding', 'Reading', 'Traveling'],
+        bio: 'Passionate about coding and exploring new technologies.',
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style= {{flexDirection:'row', }}>
+                <Image source={{ uri: user.profilePic }} style={styles.profilePic} />
+                <View>
+                    <Text style={styles.name}>{user.name}</Text>
+                    <Text style={styles.detail}>{user.city + ' | ' + user.occupation}</Text>
+                    <Text style={styles.name}>{'Within ' + user.distance + ' m'}</Text>
+                    <View style={styles.profileScoreBack}>
+                        <View style={[styles.profileScore, { width: user.profileScore * 10 }]} />
+                    </View>
+                </View>
+                <TouchableOpacity>
+                    <Text>{user.connectionStatus}</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                
+                <Text style={styles.detail}>{user.interests.join(' | ')}</Text>
+                <Text style={styles.bio}>{user.bio}</Text>
+            </View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection:'column',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        elevation: 3,
+        shadowOffset: { width: 0, height: 2 },
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        margin:10,
+        marginBottom:0,
+    },
+    profilePic: {
+        marginRight:15,
+        width: 100,
+        height: 100,
+        borderRadius: 15,
+        marginBottom: 10,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    detail: {
+        fontSize: 16,
+        marginBottom: 3,
+    },
+    profileScoreBack: {
+        width: 100,
+        borderRadius: 40,
+        height: 15,
+        backgroundColor: 'red',
+        marginBottom: 10,
+    },
+    profileScore: {
+        borderRadius: 40,
+        height: 15,
+        backgroundColor: 'blue',
+        marginBottom: 10,
+    },
+    bio: {
+        fontSize: 14,
+        marginTop: 10,
+        textAlign: 'center',
+    },
+});
+
+export default UserProfile;
--- Screens/personal.js
+++ Screens/personal.js
@@ -1,28 +1,44 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, ScrollView } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import SearchComponent from "../Components/search";
+import UserProfile from "../Components/userProfile";
 const PersonalScreen = () => {
     // const handleSearch = (searchText) => {
 
     //     console.log('Searching for:', searchText);
     //   };
 
+    
     return (
         <View style={{ flexDirection: 'column' }}>
-            <View style={{ flexDirection: 'row', margin:10 }}>
-                <SearchComponent/>
-                <Image 
-                source={require('../assets/settingsIcon.png')} 
-                style={{width:30, height:30, margin:10}}
+            <View style={{ flexDirection: 'row', margin: 10 }}>
+                <SearchComponent />
+                <Image
+                    source={require('../assets/settingsIcon.png')}
+                    style={{ width: 30, height: 30, margin: 10 }}
                 />
             </View>
-
+            <ScrollView>
+                <UserProfile />
+                <UserProfile />
+                <UserProfile />
+                <UserProfile />
+                <UserProfile />
+                <UserProfile />
+                <UserProfile />
+            </ScrollView>
         </View>
 
     );
 };
 
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default PersonalScreen;
